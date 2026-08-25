a6070225cc4cc4fd1d842b2ac98d895c66e870ca
Odysseas <odysseasptls@gmail.com>
2026-08-25 22:01:32 +0300

    Initial Commit

---
+++ MotoTripTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotoTripTracker
+//
+//  Created by Odysseas Patilis on 25/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotoTripTracker/MotoTripTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotoTripTrackerApp.swift
+//  MotoTripTracker
+//
+//  Created by Odysseas Patilis on 25/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MotoTripTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
